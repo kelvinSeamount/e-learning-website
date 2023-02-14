fa708855cb2f3eeeb523b5f76f2f57bd4f4f8ee2
KelvinSeamount <kelvinnwachukwu89@gmail.com>
2023-02-14 19:33:18 +0100

    fixed  responsive design

--- js/script.js
+++ js/script.js
@@ -20,3 +20,20 @@ for (let i = 0; i < faqButton.length; i++) {
     faqButton[i].classList.toggle("open");
   });
 }
+const menuNav = document.querySelector(".nav-menu");
+const menuButton = document.querySelector("#open-menu-btn");
+const menucloseButton = document.querySelector("#close-menu-btn");
+
+menuButton.addEventListener("click", function () {
+  menuNav.style.display = "flex";
+  menucloseButton.style.display = "inline-block";
+  menuButton.style.display = "none";
+});
+
+const closeButton = function () {
+  menuNav.style.display = "none";
+  menucloseButton.style.display = "none";
+  menuButton.style.display = "block";
+};
+
+menucloseButton.addEventListener("click", closeButton);
